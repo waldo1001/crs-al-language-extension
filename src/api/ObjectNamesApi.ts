e3162a5b1d0efd075dc5d01a43dc70317376584f
Andrzej Zwierzchowski <azwierzchowski@gmail.com>
2019-02-09 11:30:34 +0000

    NAVObject refactorin

--- src/api/ObjectNamesApi.ts
+++ src/api/ObjectNamesApi.ts
@@ -1,23 +1,31 @@
 import * as CRSApi from 'crs-al-language-extension-api';
 import { StringFunctions } from '../StringFunctions';
 import { DynamicsNAV } from '../DynamicsNAV';
-import { ObjectNameHelper } from './ObjectNameHelper';
+import { NAVObject } from '../NAVObject';
+import { Settings } from '../Settings';
 
 export class ObjectNamesApi implements CRSApi.IObjectNamesApi {
 
+    private getConfigSettings() : any {
+        return Settings.GetConfigSettings(null);
+    }
+
     public GetObjectFileName(objectType: string, objectId : string, objectName: string) : string {
-        let objectNameHelper : ObjectNameHelper = new ObjectNameHelper(objectType, objectId, objectName, '', '');
-        return objectNameHelper.objectFileNameFixed;
+        let navObject : NAVObject = new NAVObject('', this.getConfigSettings(), '');
+        navObject.setObjectProperies(objectType, objectId, objectName);
+        return navObject.objectFileNameFixed;
     }
 
     public GetObjectExtensionFileName(objectType: string, objectId : string, objectName: string, extendedObjectId : string, extendedObjectName : string) : string {
-        let objectNameHelper : ObjectNameHelper = new ObjectNameHelper(objectType, objectId, objectName, extendedObjectId, extendedObjectName);
-        return objectNameHelper.objectFileNameFixed;
+        let navObject : NAVObject = new NAVObject('', this.getConfigSettings(), '');
+        navObject.setObjectExtensionProperies(objectType, objectId, objectName, extendedObjectId, extendedObjectName);
+        return navObject.objectFileNameFixed;
     }
 
     public GetObjectExtensionName(objectType: string, objectId : string, objectName: string, extendedObjectId : string, extendedObjectName : string) : string {
-        let objectNameHelper : ObjectNameHelper = new ObjectNameHelper(objectType, objectId, objectName, extendedObjectId, extendedObjectName);
-       return objectNameHelper.objectNameFixed;
+        let navObject : NAVObject = new NAVObject('', this.getConfigSettings(), '');
+        navObject.setObjectExtensionProperies(objectType, objectId, objectName, extendedObjectId, extendedObjectName);
+        return navObject.objectNameFixed;
     }
 
 }
